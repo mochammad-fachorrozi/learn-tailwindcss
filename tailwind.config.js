00148920dbd4409692bce7dc8429d264914d5ebe
Promkes 4 <mochammadfachorrozi98@gmail.com>
2023-01-21 16:49:38 +0700

    create file part3.html, add function in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
         'oji': '#bada55',
         'coffee': '#c0ffee',
       },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
     },
   },
   plugins: [],
